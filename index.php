bffb1fa5d921f4d770b50a3b04fdf580605cf0a0
Jonathan Lessinger <jonathan.lessinger@gmail.com>
2013-07-09 02:07:08 -0400

    password auth

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head></head>
+<?php
+
+require 'password_compat-master/lib/password.php';
+// index.php
+define('RUNNING_APP', true);
+// 1. place your auth code here, or...
+define('PWFILE', "./pw/pw.txt");
+
+function verifyPW($pw){
+	$pwfile = fopen(PWFILE, 'r');
+	$line = fgets($pwfile);
+	fclose(PWFILE);
+
+	//echo "about to hash";
+    $lineLessNewLine = trim($line);  
+ //   echo "line: '" . $line . "'<br>";
+  //  echo "pw: '" . $pw . "'<br>";
+    $hash = password_hash($pw, PASSWORD_BCRYPT, array("cost" => 10));
+   // echo "hash: '" . $hash . "'";
+	return password_verify($pw, $line);
+}
+
+$msg = "";
+
+switch ($_REQUEST['page']) {
+    case 'timeclock':
+        // 2. or here
+    	if(verifyPW($_REQUEST['password'])){
+        	include 'TimeClock/index.php';
+    	} else {
+    		$msg = "Password incorrect.";
+    	}
+    	//echo "tc";
+        break;
+  /*
+    case 'do_register':
+        // 2. and here, and before every include.. and so forth.
+        include 'inc/do_register.php';
+        break;
+     */
+    default:
+    	//echo "bleh";
+    	break;
+}
+// make sure to break direct access
+  defined('RUNNING_APP') or die('Cannot access this script directly'); 
+ 
+
+echo "<body>
+<form action='./index.php?page=timeclock' method='POST'>
+Password:<input name='password' type='password'>" . $msg . "<br>
+<input type='hidden' name='page' value='timeclock'>
+<input type='submit' value='Log in'>
+</form>
+</body>
+</html>";
+
+?>
